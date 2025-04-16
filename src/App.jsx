5c28f8b1508bdcaebb8920192c9baaf17d272093
PRAKASHZZ007 <prakashtgvcplayer60@gmail.com>
2025-04-16 22:36:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,15 @@ function App() {
       id: Date.now(),
       text: input,
       isChecked: false,
+      dateTime: new Date().toLocaleString("en-GB", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true,
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      }),
     };
     setTodos([...todos, newTodo]);
     setInput("");
